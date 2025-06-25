0b7854a1c219dfa41e2d9f88a11d55a7d7e27141
Varshini <varshiniprabakaran0506@gmail.com>
2025-06-25 15:08:55 +0530

    changed app.py

--- Backend/app.py
+++ Backend/app.py
@@ -532,7 +532,6 @@ if __name__ == "__main__":
     # Start WebSocket server for clients
     threading.Thread(target=start_websocket_server, daemon=True).start()
     # Start Flask server
-    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
-
-
+    #app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
